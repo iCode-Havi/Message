e0518deed36f97827ea5539af077b148ee669f49
Akila Sachintha <akilasachintha@gmail.com>
2022-12-18 12:49:50 +0530

    Mapped Entities

--- src/main/java/com/havi/message/entity/Message.java
+++ src/main/java/com/havi/message/entity/Message.java
@@ -1,20 +1,31 @@
 package com.havi.message.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long message;
+
+    //Extract EER to type
+    private String messageType;
+    private String message;
     private String replyMessage;
+
+    private String receiverType;
+    private String adminStatus;
+    private String senderType;
 }
